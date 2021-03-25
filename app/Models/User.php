cf851dc60d8ff195aef0fbb0476f4ae9513ca954
Mathieu Richard <mathieu.richard1@outlook.fr>
2021-03-25 22:55:27 +0100

    🐛 FIX: Import in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use App\Http\Resources\AlbumResource;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
